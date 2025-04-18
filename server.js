5d51dd213f2ac3e2d4cabfe5d69a9d7fa481d839
Sam Rudd <samdrudd@gmail.com>
2025-04-18 02:24:53 -0700

    Simplify cors

--- server.js
+++ server.js
@@ -8,20 +8,7 @@ const db		    = require('./config/db');
 const port = process.argv[3] || 8000;
 const app = express();
 
-const allowedOrigins = process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : [];
-console.log(allowedOrigins);
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin) || process.env.ENV === 'dev') {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true // To allow sending cookies with CORS requests
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(bodyParser);
 app.use(cookieParser());
 
